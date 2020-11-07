c8da141fd0535e6850259006905564f3d238e3ae
dgrc <patnaapp@gmail.com>
2020-11-07 13:47:45 +0530

    home Fragment changes

---
+++ app/src/main/java/bih/nic/in/ashwin/ui/activity/entryform/HealthSubCentreEntryActivity.java
@@ -0,0 +1,16 @@
+package bih.nic.in.ashwin.ui.activity.entryform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bih.nic.in.ashwin.R;
+
+public class HealthSubCentreEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_sub_centre_entry);
+    }
+}
