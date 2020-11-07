0536acb22e3c92d65cc7475e204fdedd730746ba
patnaapp <patnaapp@gmail.com>
2020-11-07 12:59:24 +0530

    Ashaworkeractivity

---
+++ app/src/main/java/bih/nic/in/ashwin/ui/activity/AshaWorkerEntryForm_Activity.java
@@ -0,0 +1,21 @@
+package bih.nic.in.ashwin.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import bih.nic.in.ashwin.R;
+
+public class AshaWorkerEntryForm_Activity extends AppCompatActivity {
+
+    Spinner sp_work_categ,sp_work,sp_reg_name;
+    EditText edt_work_complt_date,edt_amount,edt_volume,edt_pageno,edt_slno;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asha_worker_entry_form_);
+    }
+}
--- app/src/main/java/bih/nic/in/ashwin/ui/activity/UserHomeActivity.java
+++ app/src/main/java/bih/nic/in/ashwin/ui/activity/UserHomeActivity.java
@@ -76,12 +76,12 @@ public class UserHomeActivity extends AppCompatActivity implements NavigationVie
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
