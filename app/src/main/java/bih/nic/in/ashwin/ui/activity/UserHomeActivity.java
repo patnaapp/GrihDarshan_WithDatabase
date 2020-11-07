2fdeeb712f4766ebc8fccc39221dcd35734af58f
dgrc <patnaapp@gmail.com>
2020-11-07 12:57:07 +0530

    home changes

--- app/src/main/java/bih/nic/in/ashwin/ui/activity/UserHomeActivity.java
+++ app/src/main/java/bih/nic/in/ashwin/ui/activity/UserHomeActivity.java
@@ -1,14 +1,26 @@
 package bih.nic.in.ashwin.ui.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,17 +31,21 @@ import androidx.appcompat.widget.Toolbar;
 
 
 import bih.nic.in.ashwin.R;
+import bih.nic.in.ashwin.entity.UserDetails;
+import bih.nic.in.ashwin.ui.home.HomeFragment;
 import bih.nic.in.ashwin.utility.CommonPref;
 
-public class UserHomeActivity extends AppCompatActivity {
+public class UserHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    DrawerLayout drawer;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
        /* FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +55,7 @@ public class UserHomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
         TextView navUsername = (TextView) headerView.findViewById(R.id.navUsername);
@@ -49,6 +65,7 @@ public class UserHomeActivity extends AppCompatActivity {
         navMobileNum.setText("9905104780");
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_sync, R.id.nav_logOut)
                 .setDrawerLayout(drawer)
@@ -56,8 +73,7 @@ public class UserHomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -72,5 +88,66 @@ public class UserHomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                displaySelectedFragment(new HomeFragment());
+                break;
+            case R.id.nav_sync:
+                syncData();
+                break;
+            case R.id.nav_logOut:
+                logout();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void
+    displaySelectedFragment(Fragment fragment){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+    }
+
+    private void confirmLogout(){
+        SplashActivity.prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = SplashActivity.prefs.edit();
+        editor.putBoolean("username", false);
+        editor.putBoolean("password", false);
+        editor.commit();
+
+        UserDetails userInfo = CommonPref.getUserDetails(getApplicationContext());
+        userInfo.setAuthenticated(false);
+        CommonPref.setUserDetails(this, userInfo);
+
+        Intent intent = new Intent(this,LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void logout() {
+        new AlertDialog.Builder(this)
+                .setTitle("Logout")
+                .setIcon(R.drawable.asha)
+                .setMessage("क्या आप सुनिश्चित हैं कि आप ऐप से लॉगआउट करना चाहते हैं??")
+                .setCancelable(false)
+                .setPositiveButton("हाँ", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        confirmLogout();
+                    }
+                })
+                .setNegativeButton("नहीं", null)
+                .show();
+    }
+
+    private void syncData(){
+        Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show();
     }
 }
