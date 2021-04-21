c40297cb943bc5f2988c8e625fd5265c6b08f56e
DGRC-RN <dgrc-bih@nic.in>
2021-04-21 18:49:44 +0530

    changes in year

--- app/src/main/java/bih/nic/in/ashwin/ui/activity/AshaWorkerEntryForm_Activity.java
+++ app/src/main/java/bih/nic/in/ashwin/ui/activity/AshaWorkerEntryForm_Activity.java
@@ -940,7 +940,9 @@ public class AshaWorkerEntryForm_Activity extends AppCompatActivity implements A
     public void viewCalender(){
         Calendar c = Calendar.getInstance();
         //int mYear = c.get(Calendar.YEAR);
-        int mYear =Integer.parseInt(fyear.getYear_Id())-1;
+        String year = fyear.getFinancial_year().split("-")[0];
+        //int mYear =Integer.parseInt(fyear.getYear_Id())-1;
+        int mYear =Integer.parseInt(year);
         int mnth = Integer.parseInt(fmonth.get_MonthId())-1;
         //int mMonth = c.get(Calendar.MONTH);
         int mDay = c.get(Calendar.DAY_OF_MONTH);
